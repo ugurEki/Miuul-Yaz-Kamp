dce8584937910f8b5c1a050fd63095d55e2ce37a
UgurEknci <ugur.ekinci.edu@gmail.com>
2023-07-21 01:09:36 +0300

    added kategorik değişken analizi.

---
+++ Kategorik_degisken_analizi.py
@@ -0,0 +1,39 @@
+### Kategorik değişken analizi-1
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+df = sns.load_dataset("titanic")
+
+print(df.info())
+
+cat_cols = [col for col in df.columns if str(df[col].dtypes) in ["category", "object", "bool"]]
+
+num_but_cat = [col for col in df.columns if (df[col].nunique() < 10) and str(df[col].dtypes) in ["int64", "float64"]]
+
+cat_but_cardinal = [col for col in df.columns if (df[col].nunique() > 20) and str(df[col].dtypes) in ["category", "object"]]
+
+cat_cols_new = cat_cols + num_but_cat 
+
+cat_cols_new = [col for col in cat_cols_new if col not in cat_but_cardinal]
+
+def cat_summary(dataframe, col_name, plot = False):
+
+    col_name_1 = dataframe[col_name].value_counts()
+    ratio = 100 * dataframe[col_name].value_counts() / len(dataframe)
+    print("{}: {}\nRatio: {}\n".format(col_name, col_name_1, ratio))
+    print("###########################")
+    if  dataframe[col_name].dtype == 'bool':
+            dataframe[col_name] = dataframe[col_name].astype("int")
+            print(dataframe[col_name].dtype)
+    if plot :
+            sns.countplot(x = dataframe[col_name], data=dataframe)
+            plt.show()
+        
+
+result = [cat_summary(df, col, plot = True) for col in cat_cols_new]
+
+#print(cat_summary(df, "parch", plot=True))
+print(result)
---
+++ Module_2/Gelismis_fonksiyonel_kesifci_veri_analizi.py
@@ -0,0 +1,36 @@
+### Gelişmiş Fonksiyonel Keşifçi Veri Analizi (Advanced Functional EDA)
+
+# - Genel Resim.
+# - Kategorik değişken analizi. (Analysis of categorical variables)
+# - Sayısal değişkenlerin analizi. (Analysis of numerical variables)
+# - Hedef değişkenlerin analizi. (Analysis of target variables)
+# - Korelasyon analizi. (Analysis of Correlation)
+
+## 1. Genel Resim
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+df = sns.load_dataset("titanic")
+
+def check_df(dataframe, head = 5):
+    print("############### Shape ###############")
+    print(dataframe.shape)
+    print("############### Types ###############")
+    print(dataframe.dtypes)
+    print("############### Head ###############")
+    print(dataframe.head(head))
+    print("############### Tail ###############")
+    print(dataframe.tail(head))
+    print("############### NA ###############")
+    print(dataframe.isnull().sum())
+    print("############### Quantiles ###############")
+    print(dataframe.describe([0, 0.05, 0.5, 0.95, 0.99, 1]).T)
+
+
+print(check_df(df))
+
+sns.countplot(x = df["adult_male"].astype("category"), data=df)
+plt.show()
